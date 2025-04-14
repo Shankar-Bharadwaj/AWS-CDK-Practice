407fd72689acff7d1dd938516c1d1f6bb61db7d2
Shankar Bharadwaj <bharadwajsankara@gmail.com>
2025-04-14 23:49:26 +0530

    Use of L1 construct - CfnOutput

--- py_starter/py_starter_stack.py
+++ py_starter/py_starter_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     Stack,
     aws_s3 as s3,
-    Duration
+    Duration,
+    CfnOutput
 )
 from constructs import Construct
 
@@ -11,8 +12,17 @@ class PyStarterStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # s3 bucket construct where any file in it expires after 3 days
-        s3.Bucket(self, "PyBucket", lifecycle_rules=[
+        bucket = s3.Bucket(self, "PyBucket", lifecycle_rules=[
             s3.LifecycleRule(
                 expiration = Duration.days(3)
             )
         ])
+
+
+        # Prints a placeholder value since 'cdk synth' doesn't deploy to AWS yet
+        print("Bucket Name: ", bucket.bucket_name)  # Bucket Name:  ${Token[TOKEN.23]}
+
+
+        # Outputting the bucket name using cdk
+        CfnOutput(self, "PyBucketName",
+                  value=bucket.bucket_name)
